170cd0ea3bf5dc23affa55a919fcabbe607e0a9e
Juha Leppälä <juha.leppala@eduix.fi>
2018-04-05 16:59:32 +0300

    First version of eduix-base

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,17 @@
 FROM debian:stable
+MAINTAINER Juha Leppälä "juha@eduix.fi"
+ARG JAVA_VERSION=8u161
+ARG BUILD_VERSION=b12
+ARG ORACLE_HASH=2f38c3b165be4555a1fa6e98c45e0808
+ENV ATLASSIAN_USER_ID 434
+ENV ATLASSIAN_GROUP_ID 434
+ENV JAVA_HOME /usr/java/latest
+ENV PATH $PATH:/usr/java/latest/bin
 
 # Copying the Dockerfile to the image as documentation
 COPY Dockerfile /
 COPY setup.sh /opt/sunet/setup.sh
 RUN /opt/sunet/setup.sh
+WORKDIR /opt/atlassian
 
-WORKDIR /
+USER atlassian
--- setup.sh
+++ setup.sh
@@ -16,12 +16,34 @@ apt-get update && \
       procps \
       bind9-host \
       netcat-openbsd \
+      curl \
+      tar \
+      xmlstarlet \
+      less \
+      unzip \
     && apt-get -y autoremove \
     && apt-get autoclean
 
 # Do some more cleanup to save space
 rm -rf /var/lib/apt/lists/*
 
+# Downloading and install Java
+mkdir /usr/java   
+curl -Ls -H "Cookie: oraclelicense=accept-securebackup-cookie" \
+    "http://download.oracle.com/otn-pub/java/jdk/$JAVA_VERSION-$BUILD_VERSION/$ORACLE_HASH/jdk-$JAVA_VERSION-linux-x64.tar.gz" \
+    | tar -xz --directory /usr/java --no-same-owner
+cd /usr/java
+ln -s $(ls) /usr/java/latest
+
 # Create non-privileged user that should run the apps.
-addgroup --system eduix
-adduser --system --no-create-home --ingroup eduix --shell /bin/false eduix
+groupadd -g $ATLASSIAN_GROUP_ID atlassian
+useradd -u $ATLASSIAN_USER_ID -g $ATLASSIAN_GROUP_ID -N --no-create-home -s /sbin/nologin -c "Atlassian user" atlassian
+mkdir -p /opt/atlassian && chown atlassian:atlassian /opt/atlassian && chmod 755 /opt/atlassian
+mkdir /var/atlassian && chown atlassian:atlassian /var/atlassian && chmod 755 /var/atlassian
+
+cat>/opt/atlassian/atlassian_app.sh<<EOF
+#!/bin/bash
+echo "Replace me"
+EOF
+chown atlassian:atlassian /opt/atlassian/atlassian_app.sh \
+    && chmod u+wx /opt/atlassian/atlassian_app.sh
