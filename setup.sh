b653217b8c74f48c2e965b821dced513d3cf2308
Jarkko Leponiemi <jarkko.leponiemi@eduix.fi>
2022-04-13 16:26:19 +0300

    OpenJDK 11

--- setup.sh
+++ setup.sh
@@ -21,26 +21,12 @@ apt-get update && \
       xmlstarlet \
       less \
       unzip \
-      openjdk-8-jdk \
+      openjdk-11-jdk \
     && apt-get -y autoremove \
     && apt-get autoclean
 
 # Do some more cleanup to save space
 rm -rf /var/lib/apt/lists/*
-
-# Downloading and install Java
-#mkdir /usr/java
-#curl -L -H "Cookie: oraclelicense=accept-securebackup-cookie" \
-#    "https://download.oracle.com/otn-pub/java/jdk/$JAVA_VERSION-$BUILD_VERSION/$ORACLE_HASH/jdk-$JAVA_VERSION-linux-x64.tar.gz" \
-#    -o /opt/jdk-linux-x64.tar.gz; \
-#if [[ "${JDK_SHA256_CHECKSUM}" != "$(sha256sum /opt/jdk-linux-x64.tar.gz | cut -d' ' -f1)" ]]; then 
-#    echo "ERROR: SHA256 checksum of downloaded JDK does not match!"
-#    exit 1
-#fi
-#tar -xzf /opt/jdk-linux-x64.tar.gz --directory /usr/java --no-same-owner \
-#    && rm -f /opt/jdk-linux-x64.tar.gz
-#cd /usr/java
-#ln -s $(ls) /usr/java/latest
 
 # Create non-privileged user that should run the apps.
 groupadd -g $ATLASSIAN_GROUP_ID atlassian
